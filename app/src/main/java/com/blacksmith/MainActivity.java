f02e495d3f62bfa3690fbc137b3d0b8f08c8a130
piokowalski <piokowalski@onet.pl>
2019-05-31 11:30:19 -0400

    Modelling button object, adding some logic

---
+++ app/src/main/java/com/blacksmith/MainActivity.java
@@ -0,0 +1,24 @@
+package com.blacksmith;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
