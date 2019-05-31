149c87c3e7abeaad4b088330921b38cddfaccc9b
piokowalski <piokowalski@onet.pl>
2019-05-31 11:23:13 -0400

    Switching to Java back-end - MainActivity

--- app/src/main/java/com/blacksmith/MainActivity.kt
+++ app/src/main/java/com/blacksmith/MainActivity.kt
@@ -1,20 +1,20 @@
-package com.blacksmith
-
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
-
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        addBtn.setOnClickListener()
-    }
-}
-
-private fun Button.setOnClickListener() {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-}
+//package com.blacksmith
+//
+//import android.os.Bundle
+//import android.support.v7.app.AppCompatActivity
+//import android.widget.Button
+//import kotlinx.android.synthetic.main.activity_main.*
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        addBtn.setOnClickListener()
+//    }
+//}
+//
+//private fun Button.setOnClickListener() {
+//    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//}
