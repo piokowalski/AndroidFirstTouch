c0bbb38f16876a07da9d2d71fb74e597792d6fbe
piokowalski <piokowalski@onet.pl>
2019-06-06 14:08:57 -0400

    Switching to MBP, continuing with basic syntax

--- app/src/main/java/com/blacksmith/Fundaments.kt
+++ app/src/main/java/com/blacksmith/Fundaments.kt
@@ -6,4 +6,16 @@ fun main(args: Array<String>) {
     var a = 1
     var b = 4
     println(a+b)
+
+    println("Data types:")
+
+    val data1: String = "hi" + " there!"
+    val data2: String = """
+        hi
+
+        $data1
+        """
+
+    println(data2)
+
 }
