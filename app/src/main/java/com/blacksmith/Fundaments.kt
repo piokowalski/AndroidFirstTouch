0c0147599d2278f54744c9747cd7bbf49c525e21
piokowalski <piokowalski@onet.pl>
2019-06-10 10:28:35 -0400

    Kotlin basic syntax - Collections

--- app/src/main/java/com/blacksmith/Fundaments.kt
+++ app/src/main/java/com/blacksmith/Fundaments.kt
@@ -30,7 +30,19 @@ fun main(args: Array<String>) {
     //Arrays
 
     val numbers: IntArray = intArrayOf(1, 2, 3, 4, 5)
-    println("Hey!! I am array "+numbers[2])
+    println("Hey!! I am array "+numbers[2]+"\n")
 
+    //Collections
 
+    val items = listOf(1, 2, 3, 4)
+    println("First Element of our list = "+items.first())
+    println("Last Element of our list = "+items.last())
+    println("Even Numbers of our List = "+items.
+            filter { it % 2 == 0 })   // returns [2, 4]
+
+    val readWriteMap = hashMapOf("foo" to 1, "bar" to 2)
+    println(readWriteMap["foo"])  // prints "1"
+
+    val strings = hashSetOf("a", "b", "c", "c")
+    println("My Set Values are "+strings+"\n")
 }
